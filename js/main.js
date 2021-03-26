e8f6b6e02bb15a156bb17afccddcf5c4f2c7d3a0
Hai <hou.nghoanghai@gmail.com>
2021-03-26 17:41:40 +0700

    17:41 26/03

--- js/main.js
+++ js/main.js
@@ -10,3 +10,25 @@ function showModal() {
     })
 
 }
+
+function rotateArrow() {
+    var userArrow = document.querySelector(".user-arrow");
+    if (!userArrow.classList.contains("rotate")) {
+        userArrow.classList.add("rotate")
+    } else userArrow.classList.remove("rotate");
+}
+
+function currentPage() {
+
+    var navContainer = document.querySelector(".navbar-list-menu");
+    var navItems = navContainer.querySelector(".navbar-list-item");
+
+    for (var i = 0; i < navItems.length; i++) {
+        navItems[i].addEventListener("click", function() {
+            var current = document.querySelector(".navbar-list-item.active");
+            current[0].className = current[0].className.replace("active", "");
+            this.className += "active";
+        });
+    }
+    
+}
